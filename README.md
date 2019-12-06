f7d65cc1daf2fece837e319d6dec37fb19890c05
Matt Strick <matt.strick@gmail.com>
2019-12-06 11:10:48 -0600

    mimicing what's in trumpConflictedCompanies

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# cbpConflictedCompanies
-Companies that work with Customs and Border Patrol on Trump's zero-tolerance border policy
+# customsAndBorderPatrolConflictedCompanies
+A collection of CBP conflicted companies in an easy to use JSON format.
+
+This data is a cumulative list of companies that work with Customs and Border Patrol on Trump's zero-tolerance border policy.
+
+
+## Want to help?
+### Got information that should be here? 
+Make a change request!
+
+### Got some extra change?
+Donate to these lovely places who are doing the hard work.
+
+[https://grabyourwallet.org/](https://secure.actblue.com/donate/_gywa#)
---
+++ index.js
@@ -0,0 +1,13 @@
+var CustomsAndBorderPatrolConflictedCompanies = [
+    // {
+    //   "id": "cubs",
+    //   "brand": "Cubs",
+    //   "description": "Betsy Devos owns a stake in many parts of the Cubs economy including the team and its various operations, Wrigley Field,  Comcast SportsNet Chicago, and land and property in Wrigleyville",
+    //   "sourceDescription": "2018 Financial Disclosures",
+    //   "source": "https://www.opensecrets.org/trump/financial-disclosures",
+    //   "locations": ['1060 W Addison St, Chicago, IL 60613']
+    // },
+    
+  ];
+
+  exports.CustomsAndBorderPatrolConflictedCompanies = CustomsAndBorderPatrolConflictedCompanies;
