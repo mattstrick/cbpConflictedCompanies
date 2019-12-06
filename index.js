1f0c42909d2805aa75edde9669ce97194b65c384
Matt Strick <matt.strick@gmail.com>
2019-12-06 11:52:08 -0600

    WIP: adding Grab Your Wallet sources

--- index.js
+++ index.js
@@ -7,6 +7,98 @@ var CustomsAndBorderPatrolConflictedCompanies = [
     //   "source": "https://www.opensecrets.org/trump/financial-disclosures",
     //   "locations": ['1060 W Addison St, Chicago, IL 60613']
     // },
+
+    {
+        "id": "accenture",
+        "brand": "Accenture",
+        "description": "In Feb 2018, Accenture signed a $297M contract w/ U.S. Customs and Border Protection to help hire border patrol agents.",
+        "status": "removed",
+        "source": "https://www.cnn.com/2019/04/05/politics/cbp-terminate-hiring-contract-accenture/index.html",
+        "sourceDescription": "CBP terminates controversial $297 million Accenture contract amid continued staffing struggles"
+    },
+    {
+        "id": "americanCanyonSolutions",
+        "brand": "American Canyon Solutions",
+        "description": "This company is a 'basecamp services' supplier and is currently doing business as Brookstone Emergency Services (http://www.brookstoneesd.com). It signed a contract with the Department of Health and Human Services for $2.3 million on May 23 2018. Brookstone supplies everything from tents, showers, and laundry related items to 'site management' and food service.",
+        "source": "https://readsludge.com/2019/07/03/these-nonprofits-and-businesses-are-making-millions-from-detaining-immigrant-children/",
+        "sourceDescription": "These Nonprofits and Businesses Are Making Millions From Detaining Immigrant Children"
+    },
+    {
+        "id": "brookstoneEmergencyServices",
+        "brand": "Brookstone Emergency Services",
+        "description": "This company is a 'basecamp services' supplier and is currently doing business as Brookstone Emergency Services (http://www.brookstoneesd.com). It signed a contract with the Department of Health and Human Services for $2.3 million on May 23 2018. Brookstone supplies everything from tents, showers, and laundry related items to 'site management' and food service.",
+        "source": "https://readsludge.com/2019/07/03/these-nonprofits-and-businesses-are-making-millions-from-detaining-immigrant-children/",
+        "sourceDescription": "These Nonprofits and Businesses Are Making Millions From Detaining Immigrant Children"
+    },
+    {
+        "id": "bcfs",
+        "brand": "BCFS",
+        "description": `The New York Times reports that BCFS is a global network of nonprofit groups which has received at least $179 million in federal contracts since 2015 under the government’s so-called "unaccompanied alien children program," a euphemism for detaining child migrants, a practice the UN says is always wrong.`,
+        "source": "https://www.nytimes.com/2018/06/21/us/migrant-shelters-border-crossing.html?module=inline",
+        "sourceDescription": "The Billion-Dollar Business of Operating Shelters for Migrant Children"
+    },
+    {
+        "id": "chsi",
+        "brand": "Comprehensive Health Services Inc.",
+        "description": `Comprehensive Health Services Inc. (CHSI), a Florida-based company that touts its experience with “immigrant shelter services” received the bulk of the $92 million dollars' worth of a recent round of federal contracts aimed at funding Trump's brutal immigration crackdown. News organizations have reached out to the CEO Gary Palmer for comment with no response.`,
+        "source": "https://readsludge.com/2019/07/03/these-nonprofits-and-businesses-are-making-millions-from-detaining-immigrant-children/",
+        "sourceDescription": "These Nonprofits and Businesses Are Making Millions From Detaining Immigrant Children"
+    },
+    {
+        "id": "coreCivic",
+        "brand": "Core Civic",
+        "description": `"Another one of the largest groups that runs private immigration detention centers in the United States is CoreCivic. The company runs facilities in Houston, Laredo, Dilley and Taylor." Made significant contributions to Trump's inauguration.`,
+        "source": "https://www.nytimes.com/2018/07/04/us/migrant-families-contractors-campaign-contributions.html",
+        "sourceDescription": "Some Contractors Housing Migrant Children Are Familiar to Trump’s Inner Circle"
+    },
+    {
+        "id": "dynamicServiceSolutions",
+        "brand": "Dynamic Service Solutions",
+        "description": `Dynamic Service Solutions, a Maryland firm, was awarded a contract worth up to $8.7 million from HHS through the 'shelter care for unaccompanied children' vehicle in September 2017, a euphemism for a program that includes forcible separation of kids from their parents. CEO Darnell Armstrong refused to answer repoerters' questions about his company's involvement in Trump's 'zero tolerance' crackdown.`,
+        "status": "uncertain"
+    },
+    {
+        "id": "dynamicEducationalSystems",
+        "brand": "Dynamic Educational Systems",
+        "description": `Dynamic Educational Systems, a subsidiary of the Arizona firm Exodyne, was awarded a pair of HHS contracts worth up to approximately $5.6 million for “emergency shelter operations.” CEO Ralph Rockow did not respond to reporters' requests for comment.`,
+        "status": "uncertain"
+    },
+    {
+        "id": "geoGroup",
+        "brand": "GEO Group",
+        "description": `""One of the country’s largest operators of private immigration detention facilities," reports the Dallas News. In Texas, GEO operates detention centers in Karnes City, Laredo, Pearsall and Conroe. GEO has come under scrutiny by immigrant rights organizations for alleged mismanagement and abuse in detention facilities. Made significant contributions to Trump's inauguration.`,
+        "source": "https://www.nytimes.com/2018/07/04/us/migrant-families-contractors-campaign-contributions.html",
+        "sourceDescription": "Some Contractors Housing Migrant Children Are Familiar to Trump’s Inner Circle"
+    },
+    {
+        "id": "generalDynamics",
+        "brand": "General Dynamics",
+        "description": `General Dynamics has a lucrative contract with the Office of Refuge Resettlement within the Department of Health and Human Services to help monitor youth cases as they move through the system. On May 15, 2018, it won an additional contract for more work related to immigration. This timing aligns with when the Trump administratin's "zero tolerance" crackdown began, which has included separating over 2,000 children from their parents in a six week period. Other General Dynamics jobs for ORR involve policy analysis; tracking “new placements and progress of minors in ORR funded care”; supporting ORR’s director in, among other tasks, “review[ing] files and redact[ing] information as directed.”`,
+        "source": "https://www.usaspending.gov/#/award/66259391",
+        "sourceDescription": "Government spending contract"
+    },
+    
+	
+// 12
+// 11	Hewlitt Packard	Hewlett Packard Enterprise has an active contract with ICE, according to NBC News.	CEO: antonio.neri@hpe.com // PR: neta.tully@hp.com			
+// 13
+// 12	Microsoft	Microsoft has an active contract with ICE, according to NBC News.	CEO: satyan@microsoft.com // Global Media Relations: rrt@we-worldwide.com / 425-638-7777			
+// 14
+// 13	Motorola Solutions	Motorola has an active contract with ICE, according to NBC News.	CEO: greg.brown@motorolasolutions.com // Global Media Relations, Brand: Kathryn Hanley, kathan@motorola.com			
+// 15
+// 14	MVM	Federal Contracting databases show MVM was awarded a contract worth up to $8 million over the next five years. The contract, kicked off in September 2017, calls for the company to provide assistance in emergency shelter operations for unaccompanied children and extends through September 2022. It's also been reported that MVM has multimillion-dollar contracts providing for “multiple forms of transportation, food, shelter, clothing, and other expenses related to managing these ICE and ORR programs.”	CEO Kevin Marquez: marquezk@mvminc.com			
+// 16
+// Ogilvy	A June 2019 report from Sludge.com revealed for the first time that Ogilvy has a lucrative multi-million dollar contract in place with Customs and Border Protection, which CBP later confirmed to Adweek.	cindy.luong@ogilvy.com // + 1 212 237 6158
+
+// Adweek story on Ogilvy's contract w/ CBP		
+// 17
+// 15	Palantir	Palantir, founded by Trump surrogate Peter Thiel who is also the company's largest shareholder, has an active contract with ICE, according to NBC News. Palantir "took in more than $4.9 million from ICE on May 30, part of a $39 million contract that began in 2015."	Chairman: pthiel@palantir.com, CEO: akarp@palantir.com // PR: media@palantir.com			
+// 18
+// 16	Southwest Key	The New York Times reports "Southest Key has won at least $955 million in federal contracts since 2015 to run shelters and provide other services to immigrant children in federal custody." The $240 million-a-year Southwest Key organization has big contracts with the government to house immigrant minors in its two dozen low-security shelters in Texas, Arizona, and California, a population that in recent weeks has exploded with infants and children removed from their parents." CNN reported in June 2018 that Southest Key CEO Juan Sanchez made $1.47 million a year, a salary that "reflects the organization's growth." (For perspective, the head of the American Red Cross is paid $600k per year.) In March 2019, the organization announced that Sanchez was stepping down but its immigration-related contracts remain in place. In June 2019 it was reported a Southwest Key employee was arrested for soliciting a migrant girl in a shelter.	Leadership Team: Interim CEO Joella Brooks: jbrooks@swkey.org. Rest of team: mchung@swkey.org, vdelgadosavage@swkey.org, arodriguez@swkey.org, rluna@swkey.org, jnelson@swkey.org	CNN report on CEO's salary	Vice News report on arrest of former Southwest Key employee	NYT story on Southwest Key
+// 19
+// 17	Thompson Reuters	Thompson Reuters has an active contract with ICE, according to NBC News.	Corporate Affairs: David Crundwell, david.crundwell@thomsonreuters.com Tel: +44 (0)20 7542 8763 / David Girardin, david.girardin@thomsonreuters.com / 646.223.4870			
+// 20
+// 18	Wayfair	In June 2019, 500+ Wayfair employees organized a walk-out to protest the retailer’s sale of $200,000 worth of beds and other furniture to a Texas detention center for migrant children.	PR@wayfair.com	Washington Post story	
     
   ];
 
